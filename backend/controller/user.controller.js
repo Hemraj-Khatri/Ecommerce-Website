4c00791544cb5973aabdd4bb719af73567ee94a3
Hemraj Khatri <helpingcoder78@gmail.com>
2024-06-25 07:37:41 +0545

    login added in Controller

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/user.model.js';
-// import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
  
 const signup = async(req, res, next)=>{
 try {
@@ -32,4 +32,30 @@ try {
 }
 };
 
-export {signup}
+const login = async (req, res, next) =>{
+    try {
+
+        const {email, password} =req.body;
+        //check if user exists
+        const user = await User.findOne({email});
+        if(!user){
+            let err = new Error("user with this email does not exist");
+            err.status = 400;
+            throw err;
+        }
+        // compare password
+        const isMatch  = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            let err = new Error("Invalid Password!");
+            err.status = 400;
+            throw err;
+        }
+        res.send("Login success");
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export {signup, login}
