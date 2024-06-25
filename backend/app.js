a5d0a647089c1c4fb5715c12aee881f935327e87
Hemraj Khatri <helpingcoder78@gmail.com>
2024-06-25 07:02:33 +0545

    add: signup with hash password

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 import express from 'express';
+//routers imports
+import userRouter from './routes/user.router.js';
+
 //initialize express app
 const app = express();
+//middlewares
+app.use(express.json())
+
+//routes
+app.use('/api/v1/users', userRouter);
+
 
 export {app};
---
+++ backend/controller/user.controller.js
@@ -0,0 +1,35 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+ 
+const signup = async(req, res, next)=>{
+try {
+    let{name, email, password, isAdmin} = req.body;
+    let userexits = await User.findOne({email:email});
+    if(userexits){
+        let err = new Error(`User with email ${email} already exists!`);
+        err.status = 400;
+        throw err;
+    }
+        let newuser = await User.create({
+        name, 
+        email, 
+        password:
+        isAdmin, 
+    }
+    );
+    // let {registerName, registerEmail, registeredIsAdmin} = registeruser
+    res.send({
+        message:"User registered sucessfully",
+        user:{
+            name:newuser.name,
+            email:newuser.email, 
+            isAdmin: newuser.isAdmin,
+
+        }
+    });
+} catch (err) {
+    next(err)
+}
+};
+
+export {signup}
