d392dd37f09826ba4faaf93d8bbbd30c23e3e8c6
Hemraj Khatri <helpingcoder78@gmail.com>
2024-06-30 11:27:11 +0545

    logger middleware added

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,23 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 //routers imports
 import userRouter from './routes/user.router.js';
+import notFoundHandler from "./middleware/notFoundMiddleware.js";
+import errorHandler from "./middleware/errorMiddleware.js";
+import logger from "./middleware/logger.js";
+
 
 //initialize express app
 const app = express();
 //middlewares
 app.use(express.json())
 app.use(cookieParser());
-
+app.use(logger);
 //routes
 app.use('/api/v1/users', userRouter);
 
+
+// error handlers
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export {app};
